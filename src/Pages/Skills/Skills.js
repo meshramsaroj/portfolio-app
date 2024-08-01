64deb7c572091737f07eb563fe0d85a379671f05
saroj-meshram <meshramsaroj24@gmail.com>
2024-08-01 16:58:35 +0530

    Feat: Added skill data and page

--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Row, Col, ProgressBar } from 'react-bootstrap'
+import { profileData } from '../../data'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <Row>
+      {profileData.skills.map(skillData=>(
+        <Col md={4} sm={12} className='my-2'>
+         <h4>{skillData.name}</h4>
+          <div>
+          {skillData.skillSet.map(skill=>(
+            <section className='my-2'>
+             <span className='text-capitalize'>{skill.name}</span>
+             <ProgressBar now={skill.rate} label={`${skill.rate}%`} className='border' variant='info' />
+            </section>
+          ))}
+        </div>
+        </Col>
+      ))}
+    </Row>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,84 @@
+export const profileData = {
+    skills: [
+        {
+            name: 'Front end basics',
+            skillSet: [
+                {
+                    name: "HTML",
+                    rate: 90,
+                },
+                {
+                    name: "CSS",
+                    rate: 90,
+                },
+                {
+                    name: "Javascript",
+                    rate: 70,
+                },
+                {
+                    name: "TypeScript",
+                    rate: 60
+                }
+
+            ]
+        },
+        {
+            name: "Styling frameworks",
+            skillSet: [
+                {
+                    name: "Bootstrap",
+                    rate: 90
+                },
+                {
+                    name: "tailwind css",
+                    rate: 50,
+                },
+                {
+                    name: 'Ant design',
+                    rate: 70
+                }
+            ]
+        },
+        {
+            name: 'Front end frameworks/ libraries',
+            skillSet:[
+                {
+                    name:"React",
+                    rate: 80,
+                    label: "Expert"
+                },
+                {
+                    name: 'Next',
+                    rate: 50,
+                    label: "beginner"
+                },
+                {
+                    name: 'Angular',
+                    rate: 60,
+                    label: "Intermediate"
+                },
+               
+            ]
+
+        },
+        // {
+        //     name: "Backend",
+        //     skillSet: [
+        //         {
+        //             name: "Express Js",
+        //             rate: 50,
+        //             label: "beginner"
+        //         },
+        //     ]
+        // },
+        {
+            name: "Others",
+            skillSet: [
+                {
+                    name: "Git",
+                    rate: 80,
+                }
+            ]
+        },
+    ]
+}
